cf3959b42bb59f7c3ef17e54e375e8d1d32e5199
Akash-Patil-12 <patilakash121212@gmail.com>
2021-06-30 22:31:53 +0530

    Added : Calculate Employee Wage using Switch Case

--- emp-Wage.sh
+++ emp-Wage.sh
@@ -1,17 +1,20 @@
 #!/bin/bash -x
 range=$((2-0+1))
 empCheck=$(($(($RANDOM%$range))+0))
-echo $empCheck
 empWagePerHr=20
-if [ $empCheck -eq 1 ]
-then
-  echo Employee is present
-  echo "Employee Full Day Wage is : $(($empWagePerHr*12))"
-elif [ $empCheck -eq 2 ]
-then
-  echo Employee present half day
-  echo "Employee Half Day Wage is : $(($empWagePerHr*8))"
-else
-  echo Employee is absent
-  echo "Employee is not present so Wage is : $(($empWagePerHr*0))"
-fi
+totalWorkHr=0
+case $empCheck in
+   0)
+		totalWorkHr=0
+	;;
+	1)
+		totalWorkHr=12
+   ;;
+   2)
+      totalWorkHr=8
+   ;;
+   *)
+      echo "wrong input"
+   ;;
+esac
+echo "Employee total wage is :$(($empWagePerHr*$totalWorkHr))"
