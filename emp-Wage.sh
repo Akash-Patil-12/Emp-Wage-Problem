41ada0a41b32876c50b74e967e4dec3a4ce54172
Akash-Patil-12 <patilakash121212@gmail.com>
2021-06-30 22:19:47 +0530

    Added : Employee part time wage is also calculate

--- emp-Wage.sh
+++ emp-Wage.sh
@@ -1,10 +1,16 @@
 #!/bin/bash -x
-empCheck=$((RANDOM%2))
+range=$((2-0+1))
+empCheck=$(($(($RANDOM%$range))+0))
+echo $empCheck
 empWagePerHr=20
 if [ $empCheck -eq 1 ]
 then
   echo Employee is present
-  echo "Employee Full Day Wage is : $(($empWagePerHr*8))"
+  echo "Employee Full Day Wage is : $(($empWagePerHr*12))"
+elif [ $empCheck -eq 2 ]
+then
+  echo Employee present half day
+  echo "Employee Half Day Wage is : $(($empWagePerHr*8))"
 else
   echo Employee is absent
   echo "Employee is not present so Wage is : $(($empWagePerHr*0))"
