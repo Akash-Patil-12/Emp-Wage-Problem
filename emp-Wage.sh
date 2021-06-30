7bde4ba9c4bd532bdaf2086447ddaa8d2677c78e
Akash-Patil-12 <patilakash121212@gmail.com>
2021-06-30 22:48:28 +0530

    Added : Calculating wages for a month

--- emp-Wage.sh
+++ emp-Wage.sh
@@ -1,20 +1,23 @@
 #!/bin/bash -x
-range=$((2-0+1))
-empCheck=$(($(($RANDOM%$range))+0))
-empWagePerHr=20
 totalWorkHr=0
-case $empCheck in
+empWagePerHr=20
+for ((i=1;i<=20;i++))
+do
+  range=$((2-0+1))
+  empCheck=$(($(($RANDOM%$range))+0))
+  case $empCheck in
    0)
-		totalWorkHr=0
+		totalWorkHr=$(($totalWorkHr+0))
 	;;
 	1)
-		totalWorkHr=12
+		totalWorkHr=$(($totalWorkHr+12))
    ;;
    2)
-      totalWorkHr=8
+      totalWorkHr=$(($totalWorkHr+8))
    ;;
    *)
       echo "wrong input"
    ;;
-esac
-echo "Employee total wage is :$(($empWagePerHr*$totalWorkHr))"
+  esac
+done
+echo "Employee total wage for a month is :$(($empWagePerHr*$totalWorkHr))"
