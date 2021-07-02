c3a6743bf3b3f4c724a3c4122cafa86fbbfd12e9
Akash-Patil-12 <patilakash121212@gmail.com>
2021-07-02 12:25:29 +0530

    Added : Store Daily Wage along with Total Wage

--- emp-Wage.sh
+++ emp-Wage.sh
@@ -1,32 +1,41 @@
 #!/bin/bash -x
+
 totalWorkHr=0
 empWagePerHr=20
-days=1
+declare -A totalWage
 
-function cal-TotalworkHr() {
- for((i=1;i<=20;i++))
+function cal-TotalworkHr()  {
+ for((i=0;i<20;i++))
  do
   range=$((2-0+1))
   empCheck=$(($(($RANDOM%$range))+0))
   case $empCheck in
    0)
 		totalWorkHr=$(($totalWorkHr+0))
+      dailyWage[$i]=$(($empWagePerHr*o))
 	;;
 	1)
 		totalWorkHr=$(($totalWorkHr+12))
+      dailyWage[$i]=$(($empWagePerHr*12))
    ;;
    2)
       totalWorkHr=$(($totalWorkHr+8))
+      dailyWage[$i]=$(($empWagePerHr*8))
    ;;
   esac
-  ((days=$days+1))
+  totalWage[$i]="$(($totalWorkHr*$empWagePerHr))"
   if (( $totalWorkHr >=100 ))
   then
      break;
   fi
+
  done
- echo $totalWorkHr
 }
-getTotalWorkHr=$( cal-TotalworkHr )
-echo "value : $getTotalWorkHr"
-echo "Employee total wage for a month is :$(($empWagePerHr*$getTotalWorkHr))"
+cal-TotalworkHr 
+for ((j=0;j<${#dailyWage[@]};j++))
+{
+echo "Day $(($j+1)) Wage :" ${dailyWage[j]}
+echo "Total Wage :" ${totalWage[$j]}
+
+}
+echo "Employee total wage for a month is :$(($empWagePerHr*$totalWorkHr))"
